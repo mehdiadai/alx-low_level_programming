a0234eb4090fd33fe18269447985d2799a7bef74
mehdiadai <mehdiadai04@gmail.com>
2023-03-16 13:23:33 -0700

    (

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    puts("Programming is like building a multilingual puzzle");
+    printf("'%s',%s%s\n", "with proper grammar but the outcome is a piece of art", " ", "");
     return 0;
 }
