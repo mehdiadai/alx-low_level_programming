6e2871159ce3c6ba6652195560a7ae5ce9a7945c
mehdiadai <mehdiadai04@gmail.com>
2023-03-20 09:27:47 -0700

    (

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,6 @@
-#include<stdio.h>
-/**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+#include <stdio.h>
+
+int main() {
+    printf("The outcome is a piece of art,\n");
+    return 0;
 }
